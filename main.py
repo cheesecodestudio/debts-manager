a34296f1d7e99142a41f65145d0d3c3534d161c9
Keslerth <cheesecodestudio@gmail.com>
2024-09-02 19:50:58 -0600

    Prueba numero 2

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ window = Tk()
 
 window.geometry("620x468")
 window.configure(bg = "#FFFFFF")
-window.title("Administrando mis finanzas")
+window.title("Administrando dinero")
 
 canvas = Canvas(
     window,
